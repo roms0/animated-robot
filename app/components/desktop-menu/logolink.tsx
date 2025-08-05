2464963cf9bf49b65e124e9732239ade462cb357
klimov.roman10 <timeflieswhenyouhf@internet.ru>
2025-08-05 09:52:15 +0300

    explicit locale

--- app/components/desktop-menu/logolink.tsx
+++ app/components/desktop-menu/logolink.tsx
@@ -3,10 +3,12 @@
 import { Box } from "@radix-ui/themes";
 import Link from "next/link";
 import { Logo } from "../product-logo";
+import { useLocale } from "next-intl";
 
 export const LogoLink = () => {
+  const locale = useLocale();
   return (
-    <Link href={"/"}>
+    <Link href={"/"} locale={locale}>
       <Box width={"20px"} height={"20px"}>
         <Logo />
       </Box>
